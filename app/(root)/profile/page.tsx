47b611951ce445f1da99cc3cad6f361c6d9b8565
hardik8127 <hardikkumar159515@gmail.com>
2024-04-19 16:04:59 +0530

    Finalized

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,19 +1,28 @@
-
+import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import Collection from '@/components/shared/Collection';
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
-import { getEventsByUser } from '@/lib/actions/event.actions';
+import Link from 'next/link'
+import React from 'react'
 
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-const PofilePage = async() => {
-    const { sessionClaims} = auth();
-    const userId = sessionClaims?.userId as string;
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-    const organizedEvents = await getEventsByUser({userId, page:1})
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
+
   return (
     <>
-      {/*MY Tickets */}
+      {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
@@ -24,32 +33,32 @@ const PofilePage = async() => {
           </Button>
         </div>
       </section>
-     {/* <section className="wrapper my-8">
+
+      <section className="wrapper my-8">
         <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
-          page={1}
+          page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={2}
-    
+          totalPages={orders?.totalPages}
         />
-      </section>  */}
+      </section>
 
-      {/*Events Organized */}
+      {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className='h3-bold text-center sm:text-left'>Events Oganized</h3>
+          <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events/create">
+            <Link href="/events/create">
               Create New Event
             </Link>
           </Button>
         </div>
       </section>
-      
+
       <section className="wrapper my-8">
         <Collection 
           data={organizedEvents?.data}
@@ -57,13 +66,13 @@ const PofilePage = async() => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
-  );
-};
+  )
+}
 
-export default PofilePage;
+export default ProfilePage
